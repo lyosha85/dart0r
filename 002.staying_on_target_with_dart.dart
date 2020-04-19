f01104c90983bead1223ed6aa410446912073ddb
Alex <akarpov@tractionguest.com>
2020-04-18 18:42:01 -0700

    Create deck

---
+++ 002.staying_on_target_with_dart.dart
@@ -0,0 +1,31 @@
+void main() {
+  var deck = new Deck();
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+  String name;
+
+  Deck(){
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits){
+      for (var rank in ranks) {
+        var card = new Card(suit, rank);
+        cards.add(card);
+      }
+    }
+  }
+  toString(){
+    return 'this is a Deck!';
+  }
+}
+
+class Card {
+  String rank;
+  String suite;
+
+  Card(this.rank, this.suite);
+}
